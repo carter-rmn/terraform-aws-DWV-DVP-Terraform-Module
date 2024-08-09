b0805527e3d26c9884e6ade354ce2d554ec90605
Vamshi MohanNagulapally <vamshi.mohannagulapally@petco.com>
2024-08-09 20:51:06 +0530

    update

--- modules/secrets-manager/main.tf
+++ modules/secrets-manager/main.tf
@@ -1,5 +1,4 @@
 resource "aws_secretsmanager_secret" "secret_ec2s" {
-  #for_each = local.keys
   name     = "${var.project_name}-${var.PROJECT_CUSTOMER}-${var.PROJECT_ENV}-secret-key-${each.key}"
 
   tags = {
