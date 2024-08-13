d4c6516f5d139bc9eae13016b1c8ff407ac7a27f
Vamshi MohanNagulapally <vamshi.mohannagulapally@petco.com>
2024-08-13 23:40:29 +0530

    update code

--- dw_sg.tf
+++ dw_sg.tf
@@ -172,3 +172,37 @@ resource "aws_security_group" "sg_redis" {
     Terraform   = true
   }
 }
+resource "aws_security_group" "sg_api_gateway" {
+  count                     = var.api-gateway.create ? 1 : 0
+  name        = "${var.project_name}-${var.PROJECT_CUSTOMER}-${var.PROJECT_ENV}-sg-api-gateway"
+
+  vpc_id = var.vpc.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc.cidr]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc.cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "${var.project_name}-${var.PROJECT_CUSTOMER}-${var.PROJECT_ENV}-sg-api-gateway"
+    Project     = var.project_name
+    Customer    = var.PROJECT_CUSTOMER
+    Environment = var.PROJECT_ENV
+    Terraform   = true
+  }
+}
