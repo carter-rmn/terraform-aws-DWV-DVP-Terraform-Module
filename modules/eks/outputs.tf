044a6c10b602baf3e9e8b0db68d637390a85e861
Vamshi MohanNagulapally <vamshi.mohannagulapally@petco.com>
2024-08-13 14:26:37 +0530

    update

--- modules/eks/outputs.tf
+++ modules/eks/outputs.tf
@@ -2,9 +2,12 @@ output "aws_load_balancer_controller_role_arn" {
   value = aws_iam_role.aws_load_balancer_controller.arn
 }
 
-output "sg_eks" {
+output "eks_sg" {
   value = aws_eks_cluster.data_weaver_eks_cluster.vpc_config[0].cluster_security_group_id
 }
+output "sg_eks" {
+  value       = aws_security_group.sg_eks.id
+}
 output "eks" {
-  value = module.eks
+  value = aws_eks_cluster.data_weaver_eks_cluster.id
 }
