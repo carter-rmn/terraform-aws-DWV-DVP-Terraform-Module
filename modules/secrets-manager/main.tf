ad7f4c80b80fd55cc97a78e5f3e35fb10d7bd8a6
Vamshi MohanNagulapally <vamshi.mohannagulapally@petco.com>
2024-08-13 15:52:11 +0530

    update

--- modules/secrets-manager/main.tf
+++ modules/secrets-manager/main.tf
@@ -10,3 +10,9 @@ resource "aws_secretsmanager_secret" "secret_ec2s" {
     Terraform   = true
   }
 }
+resource "aws_secretsmanager_secret_version" "secret_ec2s" {
+  for_each = var.keys
+  secret_id     = aws_secretsmanager_secret.secret_ec2s[each.key].id
+  secret_string = var.secret_string[each.key]
+  #depends_on = [aws_secretsmanager_secret.secret_ec2s]
+}
--- modules/secrets-manager/secretversion.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_secretsmanager_secret_version" "secret_ec2s" {
-  for_each = var.keys
-  secret_id     = aws_secretsmanager_secret.secret_ec2s[each.key].id
-  secret_string = var.secret_string[each.key]
-}
