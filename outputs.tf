8088e0056a5b4f9f080efbc43f32d6596f0a99e5
prakhyath-shyftlabs <129062908+prakhyath-shyftlabs@users.noreply.github.com>
2024-08-26 18:16:32 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,31 @@ output "key_pair" {
     for key, key_pair in tls_private_key.ec2s : key => key_pair.private_key_pem
   }
 }
+
+output "ec2_instance_private_ip" {
+  value       = {
+    for key, instance in aws_instance.ec2s :
+    key => instance.private_ip
+  }
+}
+
+output "ec2_instance_public_ip" {
+  value       = {
+    for key, instance in aws_instance.ec2s :
+    key => instance.public_ip
+  }
+}
+
+output "ec2_instance_private_dns" {
+  value       = {
+    for key, instance in aws_instance.ec2s :
+    key => instance.private_dns
+  }
+}
+
+output "ec2_instance_public_dns" {
+  value       = {
+    for key, instance in aws_instance.ec2s :
+    key => instance.public_dns
+  }
+}
