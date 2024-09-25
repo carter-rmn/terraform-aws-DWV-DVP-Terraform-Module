ca1cede93f7d8db2cc3fd340d0461a903fa442e7
prakhyath-shyftlabs <129062908+prakhyath-shyftlabs@users.noreply.github.com>
2024-09-25 14:13:57 +0530

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,5 +1,6 @@
 locals {
-  dwv_project_name = "${var.PROJECT_PRENAME}dwv"
+  dwv_project_name = "${var.PROJECT_PRENAME}{var.PROJECT_NAME}"
+  #dwv_project_name = "${var.PROJECT_PRENAME}dwv"
   dwv_prefix       = "${local.dwv_project_name}-${var.PROJECT_CUSTOMER}-${var.PROJECT_ENV}"
 
   keys = { for item in distinct([for item, _ in local.ec2.instances : element(split("-", item), 0)]) : item => {} }
