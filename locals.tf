d5fecf3593c48ab8c612188d92d3306eb648c523
Max <max@shyftlabs.io>
2025-02-04 16:33:26 -0500

    Added alarms

--- locals.tf
+++ locals.tf
@@ -1,7 +1,8 @@
 locals {
   dwv_project_name = "${var.PROJECT_PRENAME}${var.PROJECT_NAME}"
   #dwv_project_name = "${var.PROJECT_PRENAME}dwv"
-  dwv_prefix       = "${local.dwv_project_name}-${var.PROJECT_CUSTOMER}-${var.PROJECT_ENV}"
+  dwv_prefix   = "${local.dwv_project_name}-${var.PROJECT_CUSTOMER}-${var.PROJECT_ENV}"
+  alarm_config = jsondecode(data.local_file.alarm_config.content)
 
   keys = { for item in distinct([for item, _ in local.ec2.instances : element(split("-", item), 0)]) : item => {} }
 
@@ -13,26 +14,38 @@ locals {
     ]
   }
   ec2 = {
-      ami = ""
-      instances = {
-        mongo-1 = {
-          instance_type = ""
-          subnet_index = ""
-          volume_size = ""
-          public = ""
-        }
+    ami = ""
+    instances = {
+      mongo-1 = {
+        instance_type = ""
+        subnet_index  = ""
+        volume_size   = ""
+        public        = ""
       }
+    }
   }
   mongo = {
-      port = 27017
-      dwv_core = {
-        name = "dwvcoreDB"
-        usernames = {
-          root   = "root"
-          admin  = "admin"
-          app    = "dwv_core"
-          viewer = "viewer"
-        }
+    port = 27017
+    dwv_core = {
+      name = "dwvcoreDB"
+      usernames = {
+        root   = "root"
+        admin  = "admin"
+        app    = "dwv_core"
+        viewer = "viewer"
       }
-    }  
+    }
+  }
+  alarm = {
+    ec2 = flatten([
+      for ec2_instance, _ in var.ec2.instances : [
+        for alarm in local.alarm_config.ec2 : {
+          name     = "ec2-${ec2_instance}-${alarm.alarm_name}"
+          ec2_name = ec2_instance
+          alarm    = alarm
+        }
+      ]
+      if var.alarm.enabled
+    ])
+  }
 }
--- variable.tf
+++ variable.tf
@@ -3,124 +3,132 @@ variable "PROJECT_ENV" {}
 variable "PROJECT_PRENAME" {}
 variable "PROJECT_NAME" {}
 variable "AWS_REGION" {}
+variable "ENDPOINT_PAGERDUTY" {}
+variable "WEBHOOK_SLACK" {}
 
 variable "vpc" {
   type = object({
     vpc_id = string
-    cidr = list(string)
-    azs = list(string)
+    cidr   = list(string)
+    azs    = list(string)
     subnets = object({
-      public = list(string)
-      private = list(string)
-      database  = list(string)
+      public   = list(string)
+      private  = list(string)
+      database = list(string)
     })
   })
 }
 variable "msk" {
-    type = object({
-      create = bool
-      new = object({
-      volume_size = number
+  type = object({
+    create = bool
+    new = object({
+      volume_size            = number
       number_of_broker_nodes = number
-      instance_type = string
+      instance_type          = string
     })
     existing = object({
       bootstrap_brokers = string
     })
-    })
+  })
 }
 
 variable "key_pair" {
-    type = object({
-      keys = set(string)
-    })
+  type = object({
+    keys = set(string)
+  })
 }
 
 variable "ec2" {
   type = object({
     ami = string
     instances = map(object({
-      instance_type = string
-      subnet_index  = number
-      volume_size   = number
-      public = bool
+      instance_type    = string
+      subnet_index     = number
+      volume_size      = number
+      public           = bool
       instance_profile = string
     }))
   })
 }
 
 variable "secrets-manager" {
-    type = object({
-      keys = set(string)
-      create = bool
-    })  
+  type = object({
+    keys   = set(string)
+    create = bool
+  })
 }
 
 variable "secrets-version" {
-    type = object({
-      keys = set(string)
-      create = bool
-    })  
+  type = object({
+    keys   = set(string)
+    create = bool
+  })
 }
 
 variable "eks" {
-    type = object({
-      create = bool
-      private_id = list(string)
-      fargate_namespace_1 = string
-      fargate_namespace_2 = string
-      fargate_namespace_3 = string
-      fargate_namespace_4 = string
-      fargate_namespace_5 = string
-      aws_eks_cluster_version = string
-    })
+  type = object({
+    create                  = bool
+    private_id              = list(string)
+    fargate_namespace_1     = string
+    fargate_namespace_2     = string
+    fargate_namespace_3     = string
+    fargate_namespace_4     = string
+    fargate_namespace_5     = string
+    aws_eks_cluster_version = string
+  })
 }
 
 variable "api-gateway" {
   type = object({
-    create = bool
+    create          = bool
     certificate_arn = string
     integration_uri = string
-    domain_name = string
-    hosted_zone_id = string
-  })  
+    domain_name     = string
+    hosted_zone_id  = string
+  })
 }
 
 variable "ecr" {
-    type = object({
-      ecr = map(string)
-    })
+  type = object({
+    ecr = map(string)
+  })
 }
 
 variable "rds" {
-    type = object({
-      create = bool
-      db_name = string
-      engine = string
-      engine_version = string
-      instance_class = string
-      username = string
-      password = string
-      parameter_group_name = string
-      rds_allocated_storage = number
-      rds_max_allocated_storage = number
-    })  
+  type = object({
+    create                    = bool
+    db_name                   = string
+    engine                    = string
+    engine_version            = string
+    instance_class            = string
+    username                  = string
+    password                  = string
+    parameter_group_name      = string
+    rds_allocated_storage     = number
+    rds_max_allocated_storage = number
+  })
 }
 
 variable "s3" {
-    type = object({
-      names = list(string)
-    })
+  type = object({
+    names = list(string)
+  })
 }
 
 variable "redis" {
-    type = object({
-      create = bool
-      redis_engine_version = string
-      redis_node = string
-      redis_num_shards = number
-      redis_num_replicas_per_shard = number
-      redis_snapshot_retention_limit = number
-    })
+  type = object({
+    create                         = bool
+    redis_engine_version           = string
+    redis_node                     = string
+    redis_num_shards               = number
+    redis_num_replicas_per_shard   = number
+    redis_snapshot_retention_limit = number
+  })
 }
 
+variable "alarm" {
+  type = object({
+    enabled = bool
+    emails  = list(string)
+  })
+}
