a269cfd95d2444498704b5806b5c254e5b6acb2e
Max <max@shyftlabs.io>
2025-02-05 10:28:42 -0500

    Removed redundant code

--- locals.tf
+++ locals.tf
@@ -4,26 +4,8 @@ locals {
   dwv_prefix   = "${local.dwv_project_name}-${var.PROJECT_CUSTOMER}-${var.PROJECT_ENV}"
   alarm_config = jsondecode(data.local_file.alarm_config.content)
 
-  keys = { for item in distinct([for item, _ in local.ec2.instances : element(split("-", item), 0)]) : item => {} }
-
   msk = { bootstrap_brokers = var.msk.create ? aws_msk_cluster.kafka_cluster[0].bootstrap_brokers : var.msk.existing.bootstrap_brokers }
 
-  ecr = {
-    repositories = [
-      "",
-    ]
-  }
-  ec2 = {
-    ami = ""
-    instances = {
-      mongo-1 = {
-        instance_type = ""
-        subnet_index  = ""
-        volume_size   = ""
-        public        = ""
-      }
-    }
-  }
   mongo = {
     port = 27017
     dwv_core = {
