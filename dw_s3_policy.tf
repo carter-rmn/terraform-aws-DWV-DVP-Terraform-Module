613eaea0fb2aeb59cd0925eff0f13f38b785a366
Max Hessari <164077794+max-shyftlabs@users.noreply.github.com>
2025-04-08 14:17:26 -0400

    Enabled S3 versioning, MFA delete, AllowSSLRequestsOnly, logs

---
+++ dw_s3_policy.tf
@@ -0,0 +1,41 @@
+resource "aws_s3_bucket_public_access_block" "s3s" {
+  for_each                = { for key in var.s3.names : key => key }
+  bucket                  = aws_s3_bucket.s3s[each.key].id
+  block_public_acls       = !each.value.publicly_readable
+  block_public_policy     = !each.value.publicly_readable
+  ignore_public_acls      = !each.value.publicly_readable
+  restrict_public_buckets = !each.value.publicly_readable
+}
+
+resource "aws_s3_bucket_versioning" "s3s" {
+  for_each = { for key in var.s3.names : key => key }
+  bucket   = aws_s3_bucket.s3s[each.key].id
+
+  versioning_configuration {
+    status     = "Enabled"
+    mfa_delete = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_policy" "s3s" {
+  for_each = { for key in var.s3.names : key => key }
+  bucket   = aws_s3_bucket.s3s[each.key].id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "AllowSSLRequestsOnly"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = "s3:*"
+        Resource  = "${aws_s3_bucket.s3s[each.key].arn}/*"
+        Condition = {
+          Bool = {
+            "aws:SecureTransport" : "false"
+          }
+        }
+      }
+    ]
+  })
+}
