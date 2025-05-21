8b6fe843891ee5256d2d172b6542423027f2f78c
Amir <amir.abolhasani.1368@gmail.com>
2025-05-21 09:49:18 -0700

    aws_s3_bucket_logging

--- dw_s3_logs.tf
+++ dw_s3_logs.tf
@@ -1,11 +1,12 @@
 resource "aws_s3_bucket_logging" "s3s" {
-  for_each      = local.s3s
+  for_each      = contains(keys(aws_s3_bucket.s3s), "log") ? local.s3s : {}
   bucket        = aws_s3_bucket.s3s[each.key].id
   target_bucket = aws_s3_bucket.s3s["log"].id
   target_prefix = each.key
 }
 
 resource "aws_s3_bucket_lifecycle_configuration" "log" {
+  count  = contains(keys(aws_s3_bucket.s3s), "log") ? 1 : 0
   bucket = aws_s3_bucket.s3s["log"].id
 
   rule {
