8246a1f4211146304ce8e3c6af41d9101d21acce
prakhyath-shyftlabs <129062908+prakhyath-shyftlabs@users.noreply.github.com>
2026-04-09 18:05:36 +0530

    added pod identity tf files

---
+++ dw_eks_pod_identity_associations.tf
@@ -0,0 +1,12 @@
+resource "aws_eks_pod_identity_association" "apps" {
+  for_each = var.pod_identity.enabled ? var.pod_identity.apps : {}
+
+  cluster_name    = var.eks.create ? aws_eks_cluster.main[0].name : var.eks.existing.name
+  namespace       = var.PROJECT_ENV
+  service_account = "${local.dwv_prefix}-${each.key}"
+  role_arn        = aws_iam_role.app_role_eks[each.value.role_key].arn
+
+  tags = merge(local.common_tags, {
+    Name = "${local.dwv_prefix}-pod-identity-${each.key}"
+  })
+}
--- locals.tf
+++ locals.tf
@@ -5,7 +5,7 @@ locals {
   alarm_config = jsondecode(data.local_file.alarm_config.content)
 
   s3s = {
-    core-output-csv = { publicly_readable = false, users = [] }
+    core-output-csv = { publicly_readable = false, users = ["app"] }
     log = { publicly_readable = false, users = [] }
   }
 
@@ -39,4 +39,20 @@ locals {
       if var.alarm.enabled
     ])
   }
+
+  app_roles_eks = {
+    for user, config in var.pod_identity.users : user => config
+    if var.pod_identity.enabled
+  }
+
+  app_roles_s3 = flatten([
+    for name, config in var.pod_identity.s3s : [
+      for user in config.users : {
+        name   = name
+        user   = user
+        config = config
+      }
+      if contains(keys(local.app_roles_eks), user)
+    ]
+  ])
 }
