849776395a61e0ff3c7321bb0d0604c7d691e3eb
prakhyath-shyftlabs <129062908+prakhyath-shyftlabs@users.noreply.github.com>
2026-04-13 21:26:01 +0530

    Update cluster name reference in EKS pod identity association

--- dw_eks_pod_identity_associations.tf
+++ dw_eks_pod_identity_associations.tf
@@ -1,7 +1,7 @@
 resource "aws_eks_pod_identity_association" "apps" {
   for_each = (var.CREATE_IAM && var.pod_identity.enabled) ? var.pod_identity.apps : {}
 
-  cluster_name    = var.eks.create ? aws_eks_cluster.main[0].name : var.eks.existing.name
+  cluster_name    = var.eks.create ? aws_eks_cluster.data_weaver_eks_cluster[0].name : var.eks.existing.name
   namespace       = var.PROJECT_ENV
   service_account = "${local.dwv_prefix}-${each.key}"
   role_arn        = aws_iam_role.app_role_eks[each.value.role_key].arn
